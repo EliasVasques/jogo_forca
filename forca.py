d1c2f70f3af32e6decdf579d21f7dceb35fe184e
EliasVasques <eliasvasques74@gmail.com>
2021-12-03 07:35:47 -0300

    deixei codigo mais 'clean code'

--- forca.py
+++ forca.py
@@ -1,10 +1,10 @@
 import pygame
 
+PRETO = (0,0,0)
 class Forca:
 
-    def __init__ (self, palavra, qtd_chances):
+    def __init__ (self, palavra):
         self.palavra = palavra
-        self.qtd_chances = qtd_chances
         self.todas_tentativas = list()
         self.certo = list()
         self.errado = list()
@@ -12,55 +12,69 @@ class Forca:
     def exibir_letra (self, tela, letra, x, y):
         self.fonte = pygame.font.SysFont('arial', 20)
         letra = self.fonte.render(letra, True, (0,0,0))
-        tela.blit(letra, (x + 10, y - 20))
+        tela.blit(letra, (x, y))
 
     def exibir_palavra(self, tela):
-        x = 200  # inicial
+        x = 200
         y = 250 
-        largura = 25
-        altura = 1
+        largura_traco = 25
+        altura_traco = 1
         for letra in self.palavra:
             if letra not in self.certo:
-                pygame.draw.rect(tela, (0,0,0), (x, y, largura, altura))
+                pygame.draw.rect(tela, PRETO, (x, y, largura_traco, altura_traco))
             else:
-                self.exibir_letra(tela, letra, x, y)
-            x += 30
+                self.exibir_letra(tela, letra, x + 10, y - 20)
+            x += 30 
 
     def exibir_forca(self, tela):
-        x = 80
-        y = 135
+        x_forca = 80
+        y_forca = 135
         forca = pygame.image.load("forca.png")
         forca = pygame.transform.scale(forca, (128, 128))
-
-        cabeca = pygame.image.load("cabeça.png")
-        corpo = pygame.image.load("corpo.png")
-        perna_braco_direito = pygame.image.load("perna_braço_direito.png")
-        perna_braco_esquerdo = pygame.image.load("perna_braço_esquerdo.png")
-
-        tela.blit(forca, (x, y))
+        tela.blit(forca, (x_forca, y_forca))
 
         qtd_erros = len(self.errado)
         if qtd_erros >= 1: 
-            tela.blit(cabeca, (x+73, y+30))
+            cabeca = pygame.image.load("cabeça.png")
+            x_cabeca = 153
+            y_cabeca = 165
+            tela.blit(cabeca, (x_cabeca, y_cabeca))
         if qtd_erros >= 2:
-            tela.blit(corpo, (x+75, y+42))
+            corpo = pygame.image.load("corpo.png")
+            x_corpo = 155
+            y_corpo = 177
+            tela.blit(corpo, (x_corpo, y_corpo))
         if qtd_erros >= 3:
-            tela.blit(perna_braco_direito, (x+72, y+38))
+            braco_direito = pygame.image.load("perna_braço_direito.png")
+            x_braco_direito = 152
+            y_braco_direito = 173
+            tela.blit(braco_direito, (x_braco_direito, y_braco_direito))
         if qtd_erros >= 4:
-            tela.blit(perna_braco_direito, (x+72, y+56))
+            braco_esquerdo = pygame.image.load("perna_braço_esquerdo.png")
+            x_braco_esquerdo = 158
+            y_braco_esquerdo = 173
+            tela.blit(braco_esquerdo, (x_braco_esquerdo, y_braco_esquerdo))
         if qtd_erros >= 5:
-            tela.blit(perna_braco_esquerdo, (x+78, y+38))
+            perna_direita = pygame.image.load("perna_braço_direito.png")
+            x_perna_direita = 152
+            y_perna_direita = 191
+            tela.blit(perna_direita, (x_perna_direita, y_perna_direita))
         if qtd_erros >= 6:
-            tela.blit(perna_braco_esquerdo, (x+78, y+56))
-            self.fonte = pygame.font.SysFont('arial', 80)
-            letra = self.fonte.render("Game over", True, (0,0,0))
-            tela.blit(letra, (120, 200))
+            perna_esquerda = pygame.image.load("perna_braço_esquerdo.png")
+            x_perna_esquerda = 158
+            y_perna_esquerda = 191
+            tela.blit(perna_esquerda, (x_perna_esquerda, y_perna_esquerda))
 
     def vivo (self):
         qtd_erros = len(self.errado)
         if qtd_erros == 6:
             return False
         return True
+
+    def msg_game_over (self, tela) :
+        self.fonte = pygame.font.SysFont('arial', 80)
+        letra = self.fonte.render("Game over", True, (0,0,0))
+        tela.blit(letra, (120, 200))
     
     def tentativa (self, letra):
         if letra in self.palavra:
--- main.py
+++ main.py
@@ -4,42 +4,38 @@ from sys import exit
 
 from forca import Forca
 
-letras = {"a": K_a, "b": K_b, "c": K_c, "d": K_d, "e": K_e, "f": K_f, "g": K_g, "h": K_h, "i":K_i, "j": K_j, "k": K_k, "l": K_l, "m": K_m, "n": K_n, "o": K_o, "p": K_p, "q": K_q, "r": K_r, "s": K_s, "t": K_t, "u": K_u, "v": K_v, "w": K_w, "x": K_x, "y": K_y, "z": K_z}
-
-forca = Forca("alienigina", 10)
-
+BRANCO = (255,255,255)
+LARGURA = 640
+ALTURA = 480
 
 pygame.init()
 
+forca = Forca("alienigina")
 
-LARGURA = 640
-ALTURA = 480
 tela = pygame.display.set_mode((LARGURA, ALTURA))
 
-while True:
-    tela.fill((255,255,255))
+letras = {"a": K_a, "b": K_b, "c": K_c, "d": K_d, "e": K_e, "f": K_f, "g": K_g, "h": K_h, "i":K_i, "j": K_j, "k": K_k, "l": K_l, "m": K_m, "n": K_n, "o": K_o, "p": K_p, "q": K_q, "r": K_r, "s": K_s, "t": K_t, "u": K_u, "v": K_v, "w": K_w, "x": K_x, "y": K_y, "z": K_z}
 
-    
+
+while True:
+    tela.fill(BRANCO)
 
     forca.exibir_forca(tela)
     forca.exibir_palavra(tela)
+
+    if not forca.vivo():
+        forca.msg_game_over(tela)
         
     pygame.display.flip()
 
-    
-
-    
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
         
-        if not forca.vivo(): # não tá vivo, não adianta teclar
-            continue
-        
-        if event.type == KEYDOWN:
+        if event.type == KEYDOWN and forca.vivo():
             for letra in letras.keys(): # letras alfabeto
-                if letras[letra] == event.key:  # keydown das letras == pressionado
+                if letras[letra] == event.key:  # keydown das letras == pressionou
                     forca.tentativa(letra)
 
 
